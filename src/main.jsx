d42a3fb4ff25a8bfafd94e1e7d9c15d1f133ffce
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-07-01 23:34:49 +0600

    feat: enable authentication routes and wrap application in AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from "react-dom/client";
 // import AuthProvider from "./provider/AuthProvider.jsx";
 import { RouterProvider } from "react-router";
 import Router from "./routes/Router.jsx";
+import AuthProvider from "./provider/AuthProvider.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <AuthProvider>
       <RouterProvider router={Router} />
+    </AuthProvider>
   </StrictMode>,
 );
